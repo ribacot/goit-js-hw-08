34cdc50ba26fd7c8c16059845aca1ee1ae63ed64
ribacot <remontdiskov1979@gmail.com>
2023-05-28 00:20:57 +0300

    bgc

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/userstyle.css'
 
 console.log(galleryItems);
 
