945de71e9771775e160a7f90f8eb36d421445290
ribacot <remontdiskov1979@gmail.com>
2023-05-30 00:30:53 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,12 @@
+'use strickt'
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle/';
-var throttle = require('lodash.throttle');
-const iframe = document.querySelector('iframe');
+import throttle from 'lodash.throttle';
+// const iframe = document.querySelector('iframe');
+const timeStart = localStorage.getItem('videoplayer-current-time') ?? 0;
 
 const player = new Player('vimeo-player', {
-  width: 640,
+  id: 'vimeo-player',
 });
-const timeStart = localStorage.getItem('videoplayer-current-time') ?? 0;
 
 player
   .setCurrentTime(Number(timeStart))
@@ -29,15 +29,9 @@ player.on('play', function () {
   console.log('played the video!');
 });
 
-const setCurrentItem = _.throttle(function (e) {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
-    console.log(e.seconds);
-}, 1000
-);
-player.on('timeupdate', setCurrentItem);
-// player.on('timeupdate', function (e) {
-//   localStorage.setItem('videoplayer-current-time', e.seconds);
-// });
+player.on('timeupdate', throttle(function (e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+},1000));
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
