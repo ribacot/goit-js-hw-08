250cce396520ad96a3a78975323a3f2854b701cc
ribacot <remontdiskov1979@gmail.com>
2023-05-30 01:33:10 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,6 +6,20 @@ const player = new Player('vimeo-player');
 
 player
   .setCurrentTime(Number(timeStart) - 5)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
 player.on('play', function () {
   console.log('played the video!');
