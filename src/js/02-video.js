d8bbb38919d4c0d272424b6ed40310065d3062be
ribacot <remontdiskov1979@gmail.com>
2023-05-30 16:07:59 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,17 +2,19 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const timeStart = localStorage.getItem('videoplayer-current-time') ?? 0;
-const player = new Player('vimeo-player');
+const player = new Player('vimeo-player', {
+});
 
 player
-  .setCurrentTime(Number(timeStart) - 5)
+  .setCurrentTime(Number(timeStart))
   .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
+  console.log('Time start:', seconds);  // seconds = the actual time that the player seeked to
   })
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
         // the time was less than 0 or greater than the video’s duration
+            console.log(error.name);
         break;
 
       default:
